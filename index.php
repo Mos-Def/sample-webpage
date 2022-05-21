55789f1a4223693140dd7edee43b7e608217437c
Ryan Siegenthaler <r.d.siegs@protonmail.com>
2022-05-20 18:47:18 -0700

    new file:   index.php

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en-us">
+  <head>
+    <meta charset="UTF-8" />
+    <link href="stylesheet.css" type="text/css" rel="stylesheet"
+    title="normal" />
+    <title>Sample Page</title>
+  </head>
+    <body>
+     <header>
+       <h1>Sample</h1>
+       <img src="path/to/your/logo/here" alt="Company logo" class="logo">
+        <div>
+          <label>Home</label>
+            <button type="button">Page</button>
+        </div>
+        <div>
+          <label>Blog</label>
+            <button type="button">Page</button>
+        </div>
+        <div>
+          <label>About us</label>
+            <button type="button">Company</button>
+        </div>
+      </header>
+          <main>
+            <h2>Welcome</h2>
+            <h3>Testimony</h3><blockquote>"Random thing said about your business"</blockquote>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget auctor arcu. Et
+              iam interdum nulla vel aliquet venenatis. Phasellus nec mi turpis. Phasellus enim sapien, egestas eu volutpat et, porttitor a mi. 
+              Curabitur a augue dignissim, lacinia orci vitae, gravida nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+              Quisque congue felis quis vehicula elementum. Quisque erat nisi, interdum nec felis at, dignissim pulvinar dolor. 
+              Nulla eu dui ut tellus interdum tincidunt. Donec interdum sapien ac tristique sagittis. 
+              Maecenas quis aliquet lorem. Praesent consectetur purus quam, a faucibus ante auctor id. Vestibulum ante ipsum primis in fa
+              ucibus orci luctus et ultrices posuere cubilia curae;
+              Maecenas in lacinia lorem. Ut egestas leo arcu, a convallis velit tempus vitae. Nullam at rhoncus urna. 
+              Pellentesque varius sagittis venenatis. Suspendisse sit amet facilisis enim. Integer nec lorem neque. Aenean eget neque nisi. 
+              Praesent eu lorem convallis, gravida nisi id, pulvinar nisl. Pellentesque mattis bibendum tortor, id laoreet arcu molestie ut. 
+              Donec facilisis libero ac nisi sodales pretium. Nullam sit amet justo nibh. Integer gravida eget enim sit amet malesuada. 
+              Suspendisse potenti. Morbi malesuada turpis et massa luctus, et eleifend purus lobortis.</p>
+          </main>
+           <ul>
+             <li><a href="yourpagehere.com">Link</a></li>
+             <li><a href="anotherpage.com">Link 2</a></li>
+             <li><a href="yetanotherpage.com">Link 3</a></li>
+           </ul>
+             <footer>
+               <form action="/action_page.php" method="get">
+                   <label for="fname">Subscribe to weekly email!:</label>
+                     <input type="text" id="fname" name="fname"><br><br>
+                   <label for="lname">Have a question? Ask me!:</label>
+                     <input type="text" id="lname" name="lname"><br><br>
+                   <input type="submit" value="Submit">
+               </form> 
+                 <address>Contact info
+                 Social media info</address>
+                 <cite>samplecite</cite>
+             </footer>
+        
+          
+     </body>
+</html>
