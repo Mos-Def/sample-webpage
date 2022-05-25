c6d5c0241def0ba5c5881af126bbd15641ffe878
Ryan Siegenthaler <98730995+RyanDSiegenthaler@users.noreply.github.com>
2022-05-24 17:29:33 -0700

    Update index.php

--- php/index.php
+++ php/index.php
@@ -2,67 +2,80 @@
 <html lang="en-us">
   <head>
     <meta charset="UTF-8" />
-    <link href="stylesheet.css" type="text/css" rel="stylesheet"
+    <link href="css/stylesheet.css" type="text/css" rel="stylesheet"
     title="normal" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sample Page</title>
   </head>
     <body>
-     <header>
-       <h1>Sample</h1>
-       <img src="path/to/your/logo/here" alt="Company logo" class="logo">
-        <div>
-          <label>Home</label>
-            <button type="button">Page</button>
-        </div>
-        <div>
-          <label>Blog</label>
-            <button type="button">Page</button>
-        </div>
-        <div>
-          <label>About us</label>
-            <button type="button">Company</button>
-        </div>
-      </header>
-          <main>
-            <h2>Welcome</h2>
-            <h3>Testimony</h3><blockquote>"Random thing said about your business"</blockquote>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget auctor arcu. Et
-              iam interdum nulla vel aliquet venenatis. Phasellus nec mi turpis. Phasellus enim sapien, egestas eu volutpat et, porttitor a mi. 
-              Curabitur a augue dignissim, lacinia orci vitae, gravida nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
-              Quisque congue felis quis vehicula elementum. Quisque erat nisi, interdum nec felis at, dignissim pulvinar dolor. 
-              Nulla eu dui ut tellus interdum tincidunt. Donec interdum sapien ac tristique sagittis. 
-              Maecenas quis aliquet lorem. Praesent consectetur purus quam, a faucibus ante auctor id. Vestibulum ante ipsum primis in fa
-              ucibus orci luctus et ultrices posuere cubilia curae;
-              Maecenas in lacinia lorem. Ut egestas leo arcu, a convallis velit tempus vitae. Nullam at rhoncus urna. 
-              Pellentesque varius sagittis venenatis. Suspendisse sit amet facilisis enim. Integer nec lorem neque. Aenean eget neque nisi. 
-              Praesent eu lorem convallis, gravida nisi id, pulvinar nisl. Pellentesque mattis bibendum tortor, id laoreet arcu molestie ut. 
-              Donec facilisis libero ac nisi sodales pretium. Nullam sit amet justo nibh. Integer gravida eget enim sit amet malesuada. 
-              Suspendisse potenti. Morbi malesuada turpis et massa luctus, et eleifend purus lobortis.</p>
-          </main>
-           <ul>
-             <li><a href="yourpagehere.com">Link</a></li>
-             <li><a href="anotherpage.com">Link 2</a></li>
-             <li><a href="yetanotherpage.com">Link 3</a></li>
-           </ul>
+      <header>
+      <div class="header">
+      <h1>Header</h1>
+      </div>
+        <nav>
+          <div class="topnav">
+          <div style="border :1px solid           black;" >
+            <a             href="/php/homepage.php">Home</a>
+            <a href="/php/aboutus.php">About us</a>
+            <a             href="/php/contactform.php">Contact</a>
+         </div> 
+         </div>
+        </nav>
+        <img src="img/company-logo.png" alt="Company logo" class="logo" style="max-width:100%;height:auto;"> 
+       </header>
+         <div class ="aside">
+         <aside>
+           <small>
+             <h3>Testimony</h3>
+               <blockquote>
+                 <em>"Random thing said about your business"</em>
+               </blockquote>
+            </small>
+         </aside>
+         </div>
+           <main>
+             <section>
+               <h2>Welcome</h2>
+                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget auctor arcu. Et
+                  Curabitur a augue dignissim, lacinia orci vitae, gravida nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+                  iam interdum nulla vel aliquet venenatis. Phasellus nec mi turpis. Phasellus enim sapien, egestas eu volutpat et, porttitor a mi. 
+                  Quisque congue felis quis vehicula elementum. Quisque erat nisi, interdum nec felis at, dignissim pulvinar dolor. 
+                 Nulla eu dui ut tellus interdum tincidunt. Donec interdum sapien ac tristique sagittis. 
+                 Maecenas quis aliquet lorem. Praesent consectetur purus quam, a faucibus ante auctor id. Vestibulum ante ipsum primis in fa
+                 ucibus orci luctus et ultrices posuere cubilia curae;
+                 Maecenas in lacinia lorem. Ut egestas leo arcu, a convallis velit tempus vitae. Nullam at rhoncus urna. 
+               Pellentesque varius sagittis venenatis. Suspendisse sit amet facilisis enim. Integer nec lorem neque. Aenean eget neque nisi. 
+              </section>
+                <article>
+                  <h4>Article</h4>
+                  <p>Praesent eu lorem convallis, gravida nisi id, pulvinar nisl. Pellentesque mattis bibendum tortor, id laoreet arcu molestie ut. 
+                  Donec facilisis libero ac nisi sodales pretium. Nullam sit amet justo nibh. Integer gravida eget enim sit amet malesuada. 
+                 Suspendisse potenti. Morbi malesuada turpis et massa luctus, et eleifend purus lobortis.</p>
+               </article>
+                  <cite>samplecite</cite>  
+           </main>
+             <ul>
+               <li><a href="yourpagehere.com">Link</a></li>
+               <li><a href="anotherpage.com">Link 2</a></li>
+               <li><a href="yetanotherpage.com">Link 3</a></li>
+            </ul>
              <footer>
-                <p><b>SEND E-MAIL</b></p>
-                <form class="contact-form" action="contactform.php" method="post">
-                  <input type="text" name="name" placeholder="Full name">
-                  <input type="text" name="mail" placeholder="Your e-mail">
-                  <input type="text" name="subject" placeholder="Subject">
-                  <textarea name="message" placeholder="message"></textarea> 
-                  <button type="submit" name="submit">SEND MAIL</button>
-                   <label for="fname">Subscribe to weekly email!:</label>
-                     <input type="text" id="fname" name="fname"><br><br>
-                   <label for="lname">Have a question? Ask me!:</label>
-                     <input type="text" id="lname" name="lname"><br><br>
-                   <input type="submit" value="Submit">
-                </form> 
-                 <address>Contact info
-                 Social media info</address>
-                 <cite>samplecite</cite>
-             </footer>
-        
-          
-     </body>
+               <p><b>SEND E-MAIL</b></p>
+               <div class="contactform" style="border :1px solid black;">
+               <form class="contact-form" action="php/contactform.php" method="post">
+                 <input type="text" name="name" placeholder="Full name">
+                 <input type="text" name="mail" placeholder="Your e-mail">
+                 <input type="text" name="subject" placeholder="Subject">
+                   <textarea name="message" placeholder="message"></textarea> 
+                     <button type="submit" name="submit">SEND MAIL</button>
+                </form>
+                </div>
+                <div style="border :1px solid-black;">
+                  <address>Contact info
+                   Social media info</address>
+                </div>
+              </footer>
+    </body>
 </html>
+
+
